f321eeb1ff861c1febebdac0591c2b341362b497
levinliu <levinmhliu@gmail.com>
2021-03-02 00:07:20 +0800

    refactor code

--- http.js
+++ http.js
@@ -1,23 +1,23 @@
 function ajax(method, url, data, contentType, callback) {
   var http = new XMLHttpRequest();
-  if (method == "get"){
-    if(data){
+  if (method == "get") {
+    if (data) {
       console.err('do not support get with data');
     }
     http.open(method, url);
-    http.setRequestHeader("Content-Type",contentType);
+    http.setRequestHeader("Content-Type", contentType);
     http.send();
-  }else{
+  } else {
     http.open(method, url);
-    http.setRequestHeader("Content-Type",contentType);
-    if(data){
+    http.setRequestHeader("Content-Type", contentType);
+    if (data) {
       http.send(data)
-    }else{
+    } else {
       http.send();
     }
   }
-  http.onreadystatechange = function () {
-    if(http.readyState==4 && http.status==200){
+  http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
       callback(http.response);
     }
   }
--- server.js
+++ server.js
@@ -1,84 +1,84 @@
-var restify = require('restify');
+const restify = require('restify');
 
-const server = restify.createServer({
-  name: 'restfly',
-  version: '1.0.0'
-});
+const server = restify.createServer({name: 'restfly', version: '1.0.0'});
 
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
-// server.use(restify.urlEncodedBodyParser({ mapParams : false }));
 
-server.get('/echo/:name', function (req, res, next) {
-  res.send(req.params);
-  return next();
-});
 server.post('/test', function(req, res, next) {
   // console.log('params=' + JSON.stringify(req.params));
-  console.log('request query=' + req.query);
-  // console.log('req header=' + req.header('Content-Type'));
-  console.log('request is json' + req.is('json'));
+  console.log('request query= ' + req.query);
+  console.log('request is json ? ' + req.is('json'));
   var body = req.body;
   console.log(body);
   var data = body;
   var url = data.site;
   console.log('site=' + url);
-  // res.send('result='+url);
-  callUrl(data, res);
+  try {
+    callUrl(data, res);
+  } catch (e) {
+    console.log('fail to call site %s with error %j', url, e);
+    res.send('fail to call site ' + url + ' with error:' + e);
+  }
   return next();
 });
-function callUrl(body, res) {
+function handleServiceResponse(res, obj, originRes) {
+  if (res) {
+    console.log('%d -> %j', res.statusCode, res.headers);
+    console.log('service return: %j', obj);
+    if (res.body) {
+      originRes.send(res.body);
+    } else {
+      originRes.send(err);
+    }
+  } else {
+    console.error('Target service is probably down.');
+    originRes.send('Target service is probably down.');
+  }
+}
+function callUrl(body, originRes) {
   var site = body.site;
   var uri = body.apiUri;
   var clients = require('restify-clients');
-  var client = clients.createJsonClient({
-    url: site,
-    version: '~1.0'
-   });
-var meth = body.meth;
-console.log('meth ' + meth + ', uri:' + uri);
-var data = body.data;
-if(meth === 'GET'){
-client.get(uri, function (err, req, res2, obj) {
-  console.log('Server returned: %j', obj);
-  console.log(res2);
-  res.send(res2.body);  
-});
-}else if (meth === 'POST'){
-client.post(uri,data, function (err, req, res2, obj){
-  console.log('server return: %j', obj);
-  console.log(res2);
-  if(res2 && res2.body){
-  res.send(res2.body);
-  }else{
-    res.send(err);
-  }
-});
-}else{
-  res.send("Do not support method");
+  var client = clients.createJsonClient({url: site, version: '~1.0'});
+  var meth = body.meth;
+  console.log('meth ' + meth + ', uri:' + uri);
+  var data = body.data;
+  if (meth === 'GET') {
+    client.get(uri, function(err, req, res, obj) {
+      handleServiceResponse(res, obj, originRes);
+    });
+  } else if (meth === 'POST') {
+    client.post(uri, data, function(err, req, res, obj) {
+      handleServiceResponse(res, obj, originRes);
+    });
+  } else if (meth === 'PUT') {
+    client.put(uri, data, function(err, req, res, obj) {
+      handleServiceResponse(res, obj, originRes);
+    });
+  } else if (meth === 'DEL' || method === 'DELETE') {
+    client.del(uri, function(err, req, res, obj) {
+      handleServiceResponse(res, obj, originRes);
+    });
+  } else {
+    console.log('method [' + meth + '] do not support');
+    originRes.send('Sorry, meth ' + meth + ' is currently not supported by RESTFly.');
+  };
 }
-}
-/*
-server.get('/*', restify.plugins.serveStatic({
-  directory: './',
-  default: 'index.html'
-}));*/
-server.get('/*',
-     restify.plugins.serveStaticFiles('./', {
-     maxAge: 3600000, // this is in millisecs
-     etag: false,
-     setHeaders: function setCustomHeaders(response, requestedPath, stat) {
-             response.setHeader('restify-plugin-x', 'awesome');
-         }
-     })
-);
 
-server.listen(8081, function () {
+const dir = __dirname;
+server.get('/*', restify.plugins.serveStaticFiles(dir, {
+  maxAge: 3600000, // this is in millisecs
+  etag: false,
+  setHeaders: function setCustomHeaders(response, requestedPath, stat) {
+    response.setHeader('RESTFly', 'RESTFly');
+  }
+}));
+
+server.listen(8081, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
 var static = require('node-static');
 var http = require('http');
 var bodyParser = require('body-parser')
-
-var dir=__dirname;
